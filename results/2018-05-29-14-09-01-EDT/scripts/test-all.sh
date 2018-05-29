2b1ff2e2424bcbd778b9078b20e4f72d1b2e1003
hning86 <haining@microsoft.com>
2018-05-29 14:10:23 -0400

    Test run on 2018-05-29-14-09-01-EDT

--- results/2018-05-29-14-09-01-EDT/scripts/test-all.sh
+++ results/2018-05-29-14-09-01-EDT/scripts/test-all.sh
@@ -13,42 +13,42 @@ echo "###################################"
 echo ""
 echo ""
 echo "##### 01.interactive.e2e.py ($(date +%F-%T)) ######"
-python ./01.interactive.e2e.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./01.interactive.e2e.py
 
 echo ""
 echo ""
 echo "##### 02.lcoal.py ($(date +%F-%T)) ################"
-python ./02.local.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./02.local.py
 
 echo ""
 echo ""
 echo "##### 03.aci.py ($(date +%F-%T)) ##################"
-python ./03.aci.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./03.aci.py
 
 echo ""
 echo ""
 echo "##### 04.dsvm ($(date +%F-%T)) ####################"
-python ./04.dsvm.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./04.dsvm.py
 
 echo ""
 echo ""
 echo "##### 05.spark ($(date +%F-%T)) ###################"
-python ./05.spark.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./05.spark.py
 
 echo ""
 echo ""
 echo "##### 07.sklearn.hd.py ($(date +%F-%T)) ###########"
-python ./07.sklearn.hd.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./07.sklearn.hd.py
 
 echo ""
 echo ""
 echo "##### 08.tf.hd.py ($(date +%F-%T)) ################"
-python ./08.tf.hd.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./08.tf.hd.py
 
 echo ""
 echo ""
 echo "##### 10.o16n.py #################"
-python ./10.o16n.py
+/home/ninghai/miniconda3/envs/$env_name/bin/python ./10.o16n.py
 
 echo ""
 echo ""
